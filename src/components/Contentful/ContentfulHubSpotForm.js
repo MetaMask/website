580b6f4716351a8808391d524d52e29f80b8b738
David Nguyen <minhnc@younetco.com>
2021-10-26 17:23:49 +0700

    Update name

---
+++ src/components/Contentful/ContentfulHubSpotForm.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const ContentfulHubSpotForm = (props) => {
+  return null;
+};
+
+export default ContentfulHubSpotForm;
+
+
+ContentfulHubSpotForm.propTypes = {
+  moduleConfig: PropTypes.shape({
+  }),
+}
