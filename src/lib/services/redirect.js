6cba866e701b5288397f180ed9c1db9d2d2730ec
David Nguyen <minhnc@younetco.com>
2022-01-20 18:57:03 +0700

    Update redirects

--- src/lib/services/redirect.js
+++ src/lib/services/redirect.js
@@ -1,9 +1,9 @@
 export default `
 (function(){
   var path = location.pathname;
-  if(path === '/privacy/') {
+  if(path === '/privacy' || path === '/privacy.html' || path === '/privacy/') {
     location.href = 'https://consensys.net/privacy-policy/';
-  } else if (path === '/terms/') {
+  } else if (path === '/terms' || path === '/terms.html' || path === '/terms/) {
     location.href = 'https://consensys.net/terms-of-use/';
   }
   var slug = path.slice(1);
