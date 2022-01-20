0b27714a9af99a57c83d475f7061fa1bc8185b27
David Nguyen <minhnc@younetco.com>
2022-01-20 17:38:21 +0700

    301 Privacy Policy Redirect

--- src/lib/services/redirect.js
+++ src/lib/services/redirect.js
@@ -1,6 +1,11 @@
 export default `
 (function(){
   var path = location.pathname;
+  if(path === '/privacy/') {
+    location.href = 'https://consensys.net/privacy-policy/';
+  } else if (path === '/terms/') {
+    location.href = 'https://consensys.net/terms-of-use/';
+  }
   var slug = path.slice(1);
   var ext = slug.split('.').pop();
   if(ext !== slug && ext === 'html') {
