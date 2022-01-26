b8175eb1c0957ae96af8c9b3eef3ab97b12d4498
David Nguyen <minhnc@younetco.com>
2022-01-26 15:47:12 +0700

    Fix Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,36 +23,32 @@ if (env.errors) {
     },
     plugins: [
       {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          // The property ID; the tracking code won't be generated without it
+          trackingId: process.env.GATSBY_GA_ID,
+          // Defines where to place the tracking script - `true` in the head and `false` in the body
+          head: false,
+          // Setting this parameter is optional
+          anonymize: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**"],
+        },
+      },
+      {
         resolve: `gatsby-plugin-google-gtag`,
         options: {
           // You can add multiple tracking ids and a pageview event will be fired for all of them.
           trackingIds: [
-            "UA-37075177-6", // Google Analytics / GA
+            process.env.GATSBY_GA_ID, // Google Analytics / GA
           ],
           // This object is used for configuration specific to this plugin
           pluginConfig: {
             // Puts tracking script in the head instead of the body
             head: false,
-            // Setting this parameter is also optional
-            respectDNT: true,
             // Avoids sending pageview hits from custom paths
             exclude: ["/preview/**"],
           },
-        },
-      },
-      {
-        resolve: `gatsby-plugin-google-analytics`,
-        options: {
-          // The property ID; the tracking code won't be generated without it
-          trackingId: "UA-37075177-6",
-          // Defines where to place the tracking script - `true` in the head and `false` in the body
-          head: false,
-          // Setting this parameter is optional
-          anonymize: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**"],
         },
       },
       `gatsby-plugin-sass`,
@@ -102,9 +98,6 @@ if (env.errors) {
       {
         resolve: `gatsby-plugin-sitemap`,
         options: {
-          exclude: [
-            '/preview/',
-          ],
           query: `
           {
             site {
@@ -128,7 +121,7 @@ if (env.errors) {
             }
           }`,
           serialize: ({ site, allSitePage, allContentfulLayout }) => {
-            let privatePages = []
+            let privatePages = ['/preview/']
             allContentfulLayout.edges.map(edge => {
               privatePages.push(edge.node.slug)
             })
