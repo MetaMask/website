52b5bf7e639f3148a871bff78666d590c2697cc3
David Nguyen <minhnc@younetco.com>
2022-07-07 12:01:10 +0700

    Fix Button color

--- src/components/ArrowMiniIcon.js
+++ src/components/ArrowMiniIcon.js
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const ArrowIcon = props => {
-  const { fill = 'white', height = '13px', width = '17px' } = props
+const ArrowMiniIcon = props => {
+  const {
+    fill = 'white',
+    height = '13px',
+    width = '17px',
+    transform = '',
+  } = props
   return (
     <svg
       width={width}
       height={height}
+      transform={transform}
       viewBox="0 0 17 13"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -16,16 +22,16 @@ const ArrowIcon = props => {
         fill-rule="evenodd"
         clip-rule="evenodd"
         d="M8.4384 10.7002L11.9274 7.37133H0.657227V5.00338H11.818L8.32824 1.67433L10.0834 0L16.6572 6.20768L10.1934 12.3745L8.4384 10.7002Z"
-        fill="white"
       />
     </svg>
   )
 }
 
-export default ArrowIcon
+export default ArrowMiniIcon
 
-ArrowIcon.propTypes = {
+ArrowMiniIcon.propTypes = {
   fill: PropTypes.string,
   height: PropTypes.string,
   width: PropTypes.string,
+  transform: PropTypes.string,
 }
