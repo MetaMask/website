bcd18ea7c565f22eb1fe621485cc95738d16fe77
David Nguyen <minhnc@younetco.com>
2022-08-09 18:04:11 +0700

    Fix SyntaxError: Invalid or unexpected token

--- src/templates/ContentfulLayout.js
+++ src/templates/ContentfulLayout.js
@@ -47,7 +47,7 @@ const ContentfulLayout = props => {
     appUninstalledScript =
       'const DEV_WRITE_KEY = "PZkSwsTBxW1BrbyIYEUjFBEumGvTyjcz", PROD_WRITE_KEY = "MHae0tTVRqyHDim9qQ9ablSZpvm3Tvzc";\n' +
       'const params = new Proxy(new URLSearchParams(window.location.search), { get: (searchParams, prop) => searchParams.get(prop), });\n' +
-      'const WRITE_KEY = (params.env == ‘prod’) ? PROD_WRITE_KEY : DEV_WRITE_KEY;\n' +
+      'const WRITE_KEY = (params.env == "prod") ? PROD_WRITE_KEY : DEV_WRITE_KEY;\n' +
       '\n' +
       '!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey=WRITE_KEY;analytics.SNIPPET_VERSION="4.15.2";\n' +
       '  // only ping Segment if an id exists\n' +
