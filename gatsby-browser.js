c8412e0f4b04160f3ce0450bcd4a916c1d01ce33
viphan007 <vipt@younetgroup.com>
2022-11-02 18:00:54 +0700

    Integrate prismjs to show embed code nicely

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@
 // Import the component at the top of the file
 import React from 'react'
 import ClientSideWrapper from './src/components/ClientSideWrapper'
+require("prismjs/themes/prism.css")
 
 export const wrapPageElement = ({ element, props }) => (
   <ClientSideWrapper {...props}>{element}</ClientSideWrapper>
--- src/components/RichText.js
+++ src/components/RichText.js
@@ -18,6 +18,78 @@ export default RichText
 
 const RichTextWrapper = styled.div`
   padding: 0;
+
+  .gatsby-remark-prismjs-copy-button-container {
+    left: -3px;
+
+    & > div.gatsby-remark-prismjs-copy-button {
+      color: black;
+      padding: 3px 8px 2px;
+      opacity: 0.3;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        opacity: 1;
+        background-color: lightgray;
+      }
+
+      @media (max-width: 600px) {
+        visibility: hidden;
+      }
+    }
+  }
+
+  .gatsby-highlight {
+    position: relative;
+  }
+
+  pre[class*='language-'] {
+    font-size: 0.85rem;
+    padding: 2rem 1.5rem 1.5rem;
+  }
+
+  .gatsby-highlight pre[class*='language-']::before {
+    border-radius: 0px 0px 4px 4px;
+    color: black;
+    font-size: 0.75rem;
+    font-family: inherit;
+    font-weight: 400;
+    letter-spacing: 0.075em;
+    line-height: 1;
+    padding: 0.25rem 0.5rem;
+    position: absolute;
+    left: 1.5rem;
+    text-align: right;
+    text-transform: uppercase;
+    text-shadow: none;
+    top: 0px;
+  }
+  .gatsby-highlight pre[class='language-javascript']::before {
+    content: 'js';
+    background: #f7df1c;
+  }
+  .gatsby-highlight pre[class='language-js']::before {
+    content: 'js';
+    background: #f7df1c;
+  }
+  .gatsby-highlight pre[class='language-css']::before {
+    content: 'css';
+    background: #ff9800;
+  }
+  .gatsby-highlight pre[class~='language-html']::before {
+    content: 'html';
+    background: #005a9c;
+  }
+
+  pre {
+    background-color: #fcfaf6;
+    & > code > span {
+      background-color: transparent !important;
+    }
+  }
+  code[class='language-text'] {
+    background-color: #f5f5f5;
+  }
 `
 
 const RichTextTitle = styled.h2`
