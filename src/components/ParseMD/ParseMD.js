8bb4dcd584e258852acf3bad8e91dddbe8ba49bd
viphan007 <vipt@younetgroup.com>
2022-12-22 15:35:13 +0700

    Add mailto function for security page

--- src/components/ParseMD/ParseMD.js
+++ src/components/ParseMD/ParseMD.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import rehypeSlug from 'rehype-slug'
 
 const ParseMD = ({ children, ...props }) => {
+  useEffect(() => {
+    const element = document.getElementById('reportBugFn')
+    if (!element) return
+
+    const mailtoReportBugFn = () => {
+      const mail = document.createElement('a')
+      mail.href = 'mailto:bugbounty@consensys.net'
+      mail.click()
+    }
+
+    element.addEventListener('click', mailtoReportBugFn)
+    return () => {
+      element.removeEventListener('click', mailtoReportBugFn)
+    }
+  }, [])
+
   return (
     <ReactMarkdown rehypePlugins={[rehypeRaw, rehypeSlug]} {...props}>
       {children}
