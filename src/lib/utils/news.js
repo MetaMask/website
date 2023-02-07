78da5f291949be6e447cdaf04ab5313dee056d8c
viphan007 <vipt@younetgroup.com>
2023-02-07 10:09:26 +0700

    Fix contentful slug for metamask articles

--- src/lib/utils/news.js
+++ src/lib/utils/news.js
@@ -13,7 +13,7 @@ let getNewsUrl = news => {
   let category = 'Uncategorized'
   if (news.categories && news.categories.length)
     category = news.categories[0].name
-  return `/news/${kebabCase(category)}/${kebabCase(news.title)}/`
+  return `/news/${kebabCase(category)}/${kebabCase(news.title.toLowerCase())}/`
 }
 
 module.exports.getNewsUrl = getNewsUrl
