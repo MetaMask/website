637f4f571327317ec04d7bef8363d282669d7483
viphan007 <vipt@younetgroup.com>
2023-06-15 10:30:05 +0700

    Create assets page and pull assets from contentful to internal metamask

---
+++ src/components/Contentful/ContentfulAssetStorage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContentWrapper from '../ContentWrapper'
+import { Section } from '../StyledGeneral'
+
+const ContentfulAssetStorage = () => {
+  return (
+    <Section sectionPadding="88px">
+      <ContentWrapper>
+        <h3>Do not support preview on AssetStorage module</h3>
+      </ContentWrapper>
+    </Section>
+  )
+}
+
+export default ContentfulAssetStorage
