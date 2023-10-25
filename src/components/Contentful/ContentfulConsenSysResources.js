6da3d39910cae2a1667d49fe5c7a4e5e7296d185
viphan007 <vipt@younetgroup.com>
2023-10-25 13:48:15 +0700

    Update upcoming events section

--- src/components/Contentful/ContentfulConsenSysResources.js
+++ src/components/Contentful/ContentfulConsenSysResources.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ConsenSysResources from '../ConsenSysResources'
+import ConsensysCommunityEvent from '../ConsensysCommunityEvent'
 
 const ContentfulConsenSysResources = props => {
   const {
     moduleConfig: { categoryId, numberOfItem, linkText, link, showDate },
   } = props
+
+  if (categoryId === 'community-event')
+    return (
+      <ConsensysCommunityEvent
+        numberOfItem={numberOfItem}
+        showDate={showDate}
+        link={link}
+      />
+    )
   return (
     <ConsenSysResources
       categoryId={categoryId}
