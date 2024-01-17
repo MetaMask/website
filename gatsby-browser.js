1a7d27553bd4f6a844ae30f2421cd00f8c525e18
Kévin Chassagne <kevinchassagne@gmail.com>
2024-01-17 13:42:02 +0100

    fix: revert asyncLDProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,45 +1,36 @@
 // gatsby-browser.js
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { withLDProvider } from 'launchdarkly-react-client-sdk'
 import ClientSideWrapper from './src/components/ClientSideWrapper'
-import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk'
 import MetaMaskContextProvider from './src/Context/MetaMaskContextProvider'
 
 require('prismjs/themes/prism.css')
 require('prismjs/plugins/line-numbers/prism-line-numbers.css')
-
-const RootElement = ({ children }) => {
-  const [LDProvider, setLDProvider] = useState(null)
-
-  useEffect(() => {
-    if (!process.env.GATSBY_LD_CLIENT_ID) {
-      console.error('GATSBY_LD_CLIENT_ID is not defined')
-      return null
-    }
-
-    asyncWithLDProvider({
-      clientSideID: process.env.GATSBY_LD_CLIENT_ID,
-      context: {
-        kind: 'user',
-        key: 'metamask-user',
-      },
-    }).then(Provider => {
-      setLDProvider(() => Provider)
-    })
-  }, [])
-
-  return (
-    LDProvider && (
-      <LDProvider>
-        <MetaMaskContextProvider>{children}</MetaMaskContextProvider>
-      </LDProvider>
-    )
-  )
-}
 
 export const wrapPageElement = ({ element, props }) => (
   <ClientSideWrapper {...props}>{element}</ClientSideWrapper>
 )
 
 export const wrapRootElement = ({ element }) => {
-  return <RootElement>{element}</RootElement>
+  const clientSideID = process.env.GATSBY_LD_CLIENT_ID
+
+  // Check if the LaunchDarkly client ID is defined
+  if (!clientSideID) {
+    console.error('GATSBY_LD_CLIENT_ID is not defined')
+    return <MetaMaskContextProvider>{element}</MetaMaskContextProvider>
+  }
+
+  const LDProvider = withLDProvider({
+    clientSideID,
+    context: {
+      kind: 'user',
+      key: 'metamask-user',
+    },
+  })(({ children }) => <>{children}</>)
+
+  return (
+    <LDProvider>
+      <MetaMaskContextProvider>{element}</MetaMaskContextProvider>
+    </LDProvider>
+  )
 }
