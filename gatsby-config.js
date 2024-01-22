e11cf056998a335475981bdcef060be3d2027c94
viphan007 <106945122+viphan007@users.noreply.github.com>
2024-01-22 12:01:46 +0700

    Using javascript-based redirects (#333)

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,6 @@ if (env.errors) {
       'gatsby-transformer-remark',
       'gatsby-plugin-root-import',
       'gatsby-transformer-inline-svg',
-      'gatsby-plugin-meta-redirect',
       {
         resolve: `gatsby-source-contentful`,
         options: {
