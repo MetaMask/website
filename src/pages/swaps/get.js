27653d8debea16390aef8dbabdd9f934b0bbdcf1
Kévin Chassagne <kevinchassagne@gmail.com>
2024-02-16 19:08:06 +0000

    wip: Add SEO + Footer

--- src/pages/swaps/get.js
+++ src/pages/swaps/get.js
@@ -1,15 +1,21 @@
+import { contentfulModuleToComponent } from '../../lib/utils/moduleToComponent'
 import MultiToken from '../../components/Landing/get/multiToken'
 import Freedom from '../../components/Landing/get/freedom'
 import Leading from '../../components/Landing/get/leading'
 import Header from '../../components/Landing/get/header'
 import Intro from '../../components/Landing/get/intro'
 import Layout from '../../templates/PageLayout'
+import { graphql } from 'gatsby'
 import './get.module.scss'
 import React from 'react'
 
-const SwapsGetPage = () => {
+const SwapsGetPage = ({ data }) => {
+  const { seo, footer } = data
+
   return (
     <Layout>
+      {seo && contentfulModuleToComponent({ ...seo, pagePath: '/swaps/get/' })}
+
       <article className="page-swaps-get">
         <Header
           title="Metamask | Portfolio"
@@ -46,9 +52,23 @@ const SwapsGetPage = () => {
           btnLabel="Try Portfolio"
           btnLink="https://portfolio.metamask.io/"
         />
+
+        {footer &&
+          contentfulModuleToComponent({ ...footer, pagePath: '/swaps/get/' })}
       </article>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query($seoId: String, $footerId: String) {
+    seo: contentfulSeo(contentful_id: { eq: $seoId }) {
+      ...ContentfulSeoFields
+    }
+    footer: contentfulLayoutFooter(contentful_id: { eq: $footerId }) {
+      ...ContentfulLayoutFooterFields
+    }
+  }
+`
 
 export default SwapsGetPage
