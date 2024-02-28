3037d25afdbc683857121aae540059e0a4bc3aa5
viphan007 <vipt@younetgroup.com>
2024-02-28 18:41:07 +0700

    Remove unused class

--- src/components/PortfolioPage/Map/Sidebar/Elements/VideoButton.js
+++ src/components/PortfolioPage/Map/Sidebar/Elements/VideoButton.js
@@ -21,7 +21,7 @@ const VideoButton = props => {
           <PlayButton>
             <PlayButtonIcon />
             <svg
-              className={CSS.border}
+              // className={CSS.border}
               viewBox="0 0 100 100"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
