fba9e7f35f915d8718af03286bfba6010596e27c
viphan007 <vipt@younetgroup.com>
2024-03-28 09:33:21 +0700

    Fix minified react error issue by launchdarkly

--- src/components/ClientSideWrapper/Wrapper.js
+++ src/components/ClientSideWrapper/Wrapper.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-computed-key */
 import React, { useEffect, useState } from 'react'
 import ContextClientSide from '../../Context/ContextClientSide'
+import MetaMaskContextProvider from '../../Context/MetaMaskContextProvider'
 
 const ClientSideWrapper = ({ children }) => {
   const [theme, setTheme] = useState(window?.__theme || 'light')
@@ -30,9 +31,11 @@ const ClientSideWrapper = ({ children }) => {
     },
   }
   return (
-    <ContextClientSide.Provider value={valueContext}>
-      {children}
-    </ContextClientSide.Provider>
+    <MetaMaskContextProvider>
+      <ContextClientSide.Provider value={valueContext}>
+        {children}
+      </ContextClientSide.Provider>
+    </MetaMaskContextProvider>
   )
 }
 
