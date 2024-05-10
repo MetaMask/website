84f133afee2c5337cbde3998963fbb81fb6d8c14
vinhtn-yns <vinhtn@younetgroup.com>
2024-05-10 15:01:35 +0700

    fix: SEO - Properly Size and Add Dimensions to Images

--- src/components/Logo/LogoImage.js
+++ src/components/Logo/LogoImage.js
@@ -5,7 +5,7 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { parseContentfulAssetUrl } from '../../lib/utils/urlParser'
 import classnames from 'classnames'
 
-const LogoImage = ({ alt, src, width, height, srcDarkMode, previewMode }) => {
+const LogoImage = ({ alt = '', src, width, height, srcDarkMode, previewMode }) => {
   return (
     <StyledPartnerImageContainer className={'LogoImageWrapper'}>
       {getImage(src) ? (
