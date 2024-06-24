9cd37debb9f0ab1fb5a7803fda2d5241e8fb6d4e
blaiseup <balazs.mozes@consensys.net>
2024-06-24 15:10:21 +0200

    fix: remove console log

--- src/hooks/useIsUKBlocked.js
+++ src/hooks/useIsUKBlocked.js
@@ -3,8 +3,6 @@ import { useCountry } from './useCountry'
 export const useIsUKBlocked = () => {
   const country = useCountry()
 
-  console.log('process.env.GATSBY_ENVIRONMENT', process.env.GATSBY_ENVIRONMENT)
-
   // Only apply geoblocking in production
   return country === 'GB' && process.env.GATSBY_ENVIRONMENT !== 'staging'
 }
