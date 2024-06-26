6a2588c6e8bb6c9bde97afd593eaaba906468885
viphan007 <vipt@younetgroup.com>
2024-06-26 10:36:41 +0700

    Update preview feature

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -187,6 +187,7 @@ const PreviewPage = () => {
             data={moduleConfig}
             locale={locale.code}
             sharedCopy={sharedCopy}
+            previewMode
           />
         )
       default:
