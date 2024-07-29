9f72e45a86ec4bc767daa70ab3b00f9f08c70080
blaiseup <balazs.mozes@consensys.net>
2024-07-29 18:38:53 +0300

    feat: add tracking for tracking navbar view portfolio cta clicks on homepage

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -2,6 +2,7 @@ import { isAndroid, isIOS, isMobile, browserName } from 'react-device-detect'
 import { contentfulModuleToComponent } from '../lib/utils/moduleToComponent'
 import { persistDeveloper } from '../lib/utils/localStorage'
 import { useLDClient } from 'gatsby-plugin-launchdarkly'
+import { useLocation } from '@reach/router'
 import useIsChromium from '../lib/utils/isChromium'
 import lowerCase from 'lodash/lowerCase'
 import React, { useEffect, useMemo, useState } from 'react'
@@ -18,6 +19,7 @@ import Image from './Image'
 import { gsap } from 'gsap'
 import Link from './Link'
 import { useLaunchDarklyFlag } from '../Context/LaunchDarklyFlagContext'
+import { removeLanguageCode } from '../lib/utils/removeLanguageCode'
 
 const CTA = props => {
   const {
@@ -72,6 +74,8 @@ const CTA = props => {
   const [iconBrowser, setIconBrowser] = useState('')
   const [useTextTreatment, setUseTextTreatment] = useState(false)
 
+  const location = useLocation()
+
   useEffect(() => {
     if (useTextTreatment) {
       setText(textTreatment)
@@ -123,6 +127,14 @@ const CTA = props => {
     if (flagName === 'home-portfolio-cta-test') {
       ldClient?.track('home-portfolio-cta-click')
       ldClient?.flush()
+    }
+
+    if (flagName === 'navbar-view-portfolio-cta') {
+      const currentPath = removeLanguageCode(location?.pathname)
+      if (currentPath === '/') {
+        ldClient?.track('navbar-view-portfolio-cta-click')
+        ldClient?.flush()
+      }
     }
 
     if (
