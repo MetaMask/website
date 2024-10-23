3aaf0ceca15269ea03dcadefe6e81b27243c3512
Kévin Chassagne <kevinchassagne@gmail.com>
2024-10-23 12:11:39 +0200

    fix: web3 slug

--- src/lib/utils/news.js
+++ src/lib/utils/news.js
@@ -1,22 +1,34 @@
 const { kebabCase } = require('lodash')
+
 /**
- *
+ * Custom kebab case function that preserves "web3"
+ * @param {string} string
+ * @returns {string}
+ */
+function customKebabCase(string) {
+  // Apply lodash's kebabCase
+  string = kebabCase(string)
+
+  // Restore "web3"
+  return string.replace(/web-3/gi, 'web3')
+}
+
+/**
  * @function
- *
  * @name getNewsUrl
- *
  * @param {Object} news
- *
  * @return {String} final news url
  */
 let getNewsUrl = news => {
   let category = 'Uncategorized'
   const slug = news?.slug
-    ? kebabCase(news.slug)
-    : kebabCase(news.title.toLowerCase())
+    ? customKebabCase(news.slug)
+    : customKebabCase(news.title.toLowerCase())
+
   if (news.categories && news.categories.length)
     category = news.categories[0].slug
-  return `/news/${kebabCase(category)}/${slug}/`
+
+  return `/news/${customKebabCase(category)}/${slug}/`
 }
 
 module.exports.getNewsUrl = getNewsUrl
