bdc1b486ace7ed9f546591c518b8dbb387e046df
Kévin Chassagne <kevin.chassagne@consensys.net>
2025-03-28 11:59:32 +0100

    Update html.js

--- src/html.js
+++ src/html.js
@@ -20,7 +20,6 @@ const HTML = props => {
     <html {...htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
-        <meta httpEquiv="refresh" content="0; url=https://metamask.io" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
